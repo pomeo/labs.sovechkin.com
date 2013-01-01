f13d8a9f47b96f105ffdbe8ff67bba98218dbc80
Sergey Ovechkin <me@sovechkin.com>
2013-01-01 19:00:47 +0400

    video chat based on opentok api

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ var express = require('express')
   , routes = require('./routes')
   , avatar = require('./routes/avatar')
   , ask = require('./routes/ask')
+  , chat = require('./routes/chat')
   , http = require('http')
   , path = require('path');
 
-var app = express();
+var app = express()
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server, { log: false });
+
+io.configure(function() {
+  io.set('close timeout', 60*60*24);
+});
 
 app.enable('trust proxy');
 
@@ -32,7 +39,10 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/avatar', avatar.img);
 app.get('/ask', ask.index);
+app.get('/chat', chat.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+chat.start(io.sockets);
